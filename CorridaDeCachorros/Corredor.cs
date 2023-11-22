e60fe9deb53d53891aad7809ae7ce21e7c361cc6
mooshradoomdlages@gmail.com <mooshradoomdlages@gmail.com>
2023-11-22 20:18:09 -0300

    #corridadecachorro

---
+++ CorridaDeCachorros/Corredor.cs
@@ -0,0 +1,27 @@
+﻿namespace CorridaDeCachorros;
+
+public class Corredor : BaseModel
+{
+    private double _distanciaPercorrida { get; set; }
+    private static readonly Random Random = new();
+    public Posicoes Posicao { get; set; }
+
+    public Corredor(int posicaoCorredor) : base()
+    {
+        Nome = $"Corredor-{posicaoCorredor}";
+        _distanciaPercorrida = 0.0;
+        Posicao = Posicoes.NaoGanho;
+    }
+
+    public void Mover()
+    {
+        int distanciaPercorrida = Random.Next(1, 6);
+
+        _distanciaPercorrida += (distanciaPercorrida * 0.1);
+    }
+
+    public double DistanciaPercorrida()
+    {
+        return _distanciaPercorrida;
+    }
+}
